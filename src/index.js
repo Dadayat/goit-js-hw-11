321d4b80c0dbb08be12fb1823aa228096f3b0eb7
Dadayat <136356333+Dadayat@users.noreply.github.com>
2023-11-16 22:47:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ function onSubmitForm(event) {
     .toLowerCase()
     .split(' ')
     .join('+');
-  // console.log(keyOfSearchPhoto);
 
   if (keyOfSearchPhoto === '') {
     btnLoadMore.classList.add('is-hidden');
@@ -58,10 +57,9 @@ function onSubmitForm(event) {
       console.log(searchResults);
       const numberOfPage = Math.ceil(data.totalHits / perPage);
       createMarkup(searchResults);
+          
         if (page >= numberOfPage) {
-
             btnLoadMore.classList.add('is-hidden');
-
         Notify.info(
           "We're sorry, but you've reached the end of search results.",
           paramsForNotify
@@ -70,90 +68,30 @@ function onSubmitForm(event) {
         btnLoadMore.classList.remove('is-hidden');
         window.addEventListener('scroll', showLoadMorePage);
         btnLoadMore.addEventListener('click', onClickLoadMore);
-        //   event.currentTarget.reset();
-      }
-      lightbox.refresh();
-      scrollPage();
-    })
-    .catch(onFetchError);
+        event.currentTarget.reset();
+      }; lightbox.refresh();
+    })  
+    .catch(er => console.log(er));
+
 }
 
-// function onSubmitForm(event) {
-//     event.preventDefault();
+btnLoadMore.addEventListener('click', onClickLoadMore);
+    
+function onClickLoadMore() {
+    page += 1;
+    fetchPhoto(keyOfSearchPhoto, page, perPage)
+        .then(data => {
+            const searchResults = data.hits;           
+          createMarkup(searchResults);
+            if (page === 1 && searchResults.length < 40) {
+                    btnLoadMore.classList.add('is-hidden');
+                    Notify.info("We're sorry, but you've reached the end of search results.", paramsForNotify);
+                    btnLoadMore.removeEventListener('click', onClickLoadMore);
+                    window.removeEventListener('scroll', showLoadMorePage);
+            }; lightbox.refresh();
+            })     
+    .catch(er => console.log(er));
 
-//     gallery.innerHTML = '';
-//     page = 1;
-//     const { searchQuery } = event.currentTarget.elements;
-//     keyOfSearchPhoto = searchQuery.value
-//         .trim()
-//         .toLowerCase()
-//         .split(' ')
-//         .join('+');
-//     // console.log(keyOfSearchPhoto);
-
-//     if (keyOfSearchPhoto === '') {
-//         Notify.info('Enter your request, please!', paramsForNotify);
-//         return;
-//     }
-
-//     fetchPhoto(keyOfSearchPhoto, page, perPage)
-//         .then(data => {
-//             const searchResults = data.hits;
-//             if (data.totalHits === 0) {
-//                 Notify.failure('Sorry, there are no images matching your search query. Please try again.', paramsForNotify);
-//             } else {
-//                 Notify.info(`Hooray! We found ${data.totalHits} images.`, paramsForNotify);
-//                 console.log(searchResults);
-//                 createMarkup(searchResults);
-//                 lightbox.refresh();
-
-//             };
-//             if (data.totalHits > perPage) {
-//                 btnLoadMore.classList.remove('is-hidden');
-//                 window.addEventListener('scroll', showLoadMorePage);
-//             };
-//             // scrollPage();
-//         })
-//         .catch(onFetchError);
-
-//     btnLoadMore.addEventListener('click', onClickLoadMore);
-
-//     event.currentTarget.reset();
-// };
-
-// function onClickLoadMore() {
-//     page += 1;
-//     fetchPhoto(keyOfSearchPhoto, page, perPage)
-//         .then(data => {
-//             const searchResults = data.hits;
-//             const numberOfPage = Math.ceil(data.totalHits / perPage);
-            
-//             createMarkup(searchResults);
-//             if (page === 1 && searchResults.length < 40) {
-//                     btnLoadMore.classList.add('is-hidden');
-//                     Notify.info("We're sorry, but you've reached the end of search results.", paramsForNotify);
-//                     btnLoadMore.removeEventListener('click', onClickLoadMore);
-//                     window.removeEventListener('scroll', showLoadMorePage);
-//             };
-            
-//                 lightbox.refresh();
-//                 // scrollPage();
-//             })
-//         .catch(onFetchError);
-// };
-
-
-// function onFetchError() {
-//     Notify.failure('Oops! Something went wrong! Try reloading the page or make another choice!', paramsForNotify);
-// };
-
-function scrollPage() {
-    const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-    });
 };
 
 function showLoadMorePage() {
